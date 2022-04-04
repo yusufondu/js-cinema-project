a4a1f7a5b9e25e243c3f20462518b149f4af4f69
Yusuf Öndü <yusuf.ondu@kariyer.net>
2022-04-04 12:56:25 +0300

    some bugs fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const slidePage = document.querySelector(".slidepage");
 const firstNextBtn = document.querySelector(".firstNextBtn");
-const prevBtnSec = document.querySelector(".prev-1");
+const prevBtnOne = document.querySelector(".prev-1");
 const nextBtnSec = document.querySelector(".next-1");
 const prevBtnThird = document.querySelector(".prev-2");
 const submitBtn = document.querySelector(".submit");
@@ -8,39 +8,62 @@ const progressText = document.querySelectorAll(".step p");
 const progressCheck = document.querySelectorAll(".step .check");
 const bullet = document.querySelectorAll(".step .bullet");
 const container = document.querySelector(".screen-container");
-const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.querySelector("#count");
 const title = document.querySelector("#title");
 const firstName = document.querySelector("#name");
 const lastName = document.querySelector("#lastName");
 const email = document.querySelector("#email");
 const seat = document.querySelector("#seat");
+const allSeats = document.querySelector("#allSeats");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 
-populateUI();
+const takePersonalData = () => {
+  if (localStorage.getItem("formData")) {
+    const deneme = JSON.parse(localStorage.getItem("formData"));
+    title.value = deneme.title;
+    firstName.value = deneme.firstName;
+    lastName.value = deneme.lastName;
+    email.value = deneme.email;
+    seat.value = deneme.seat;
+  }
+};
+window.onload = takePersonalData();
 
-// Update Count
+const progressBarAdd = () => {
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+};
+
+firstNextBtn.addEventListener("click", function (event) {
+  if (
+    title.value &&
+    firstName.value &&
+    lastName.value &&
+    email.value &&
+    seat.value > 0 &&
+    seat.value <= 100
+  ) {
+    // event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    progressBarAdd();
+  }
+});
+
 function updateSelectedCount() {
+  const seats = document.querySelectorAll(".row .seat:not(.occupied)");
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
   const seatsIndex = [...selectedSeats].map((seat) => {
-    return [...seats].indexOf(seat);
+    return [...seats].indexOf(seat) + 1;
   });
+
+  console.log("seatsIndex", seatsIndex);
 
   localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
-}
-
-// Get data from localstorage and populate the ui
-function populateUI() {
-  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
-  if (selectedSeats !== null && selectedSeats.length > 0) {
-    seats.forEach((seat, index) => {
-      if (selectedSeats.indexOf(index) > -1) {
-        seat.classList.add("selected");
-      }
-    });
-  }
 }
 
 let countSeat = 0;
@@ -67,12 +90,45 @@ container.addEventListener("click", (e) => {
   updateSelectedCount();
 });
 
-const progressBarAdd = () => {
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-};
+prevBtnOne.addEventListener("click", function (event) {
+  // event.preventDefault();
+  slidePage.style.marginLeft = "0%";
+  progressBarRemove();
+});
+
+nextBtnSec.addEventListener("click", function (event) {
+  // event.preventDefault();
+  slidePage.style.marginLeft = "-50%";
+  progressBarAdd();
+});
+
+populateUI();
+
+// Get data from localstorage and populate the ui
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  console.log("selectedSeats", selectedSeats);
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+}
+
+console.log(localStorage.getItem("selectedSeats"));
+
+for (let i = 0; i <= 10; i++) {
+  const seatRow = document.createElement("div");
+  seatRow.classList.add("row");
+  allSeats.appendChild(seatRow);
+  for (let j = 0; j <= 10; j++) {
+    const seatCol = document.createElement("div");
+    seatCol.classList.add("seat");
+    seatRow.appendChild(seatCol);
+  }
+}
 
 const progressBarRemove = () => {
   bullet[current - 2].classList.remove("active");
@@ -83,39 +139,7 @@ const progressBarRemove = () => {
 
 let current = 1;
 
-firstNextBtn.addEventListener("click", function (event) {
-  if (
-    title.value &&
-    firstName.value &&
-    lastName.value &&
-    email.value &&
-    seat.value
-  ) {
-    event.preventDefault();
-    slidePage.style.marginLeft = "-25%";
-    progressBarAdd();
-  }
-});
-
-nextBtnSec.addEventListener("click", function (event) {
-  event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
-  progressBarAdd();
-});
-
-submitBtn.addEventListener("click", function (event) {
-  progressBarAdd();
-
-  setTimeout(() => {
-    alert(`You are successfuly created your own ticket`);
-  }, 800);
-});
-
-prevBtnSec.addEventListener("click", function (event) {
-  event.preventDefault();
-  slidePage.style.marginLeft = "0%";
-  progressBarRemove();
-});
+updateSelectedCount();
 
 prevBtnThird.addEventListener("click", function (event) {
   event.preventDefault();
@@ -123,28 +147,7 @@ prevBtnThird.addEventListener("click", function (event) {
   progressBarRemove();
 });
 
-updateSelectedCount();
-
-// -------------------------------------------------------------
-
-const signUp = (e) => {
-  const formData = {
-    title: title.value,
-    firstName: firstName.value,
-    lastName: lastName.value,
-    email: email.value,
-    seat: seat.value,
-  };
-  localStorage.setItem("formData", JSON.stringify(formData));
-  // console.log(localStorage.getItem('formData'));
-  displayData();
-
-  e.preventDefault();
-  window.localStorage.clear();
-};
-
 function displayData() {
-  console.log(JSON.parse(localStorage.getItem("formData")));
   if (localStorage.getItem("formData")) {
     const { title, firstName, lastName, email, seat } = JSON.parse(
       localStorage.getItem("formData")
@@ -174,4 +177,27 @@ function displayData() {
     `;
   }
 }
-displayData();
+
+const signUp = (e) => {
+  const formData = {
+    title: title.value,
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    seat: seat.value,
+  };
+
+  localStorage.setItem("formData", JSON.stringify(formData));
+
+  displayData();
+  e.preventDefault();
+};
+
+submitBtn.addEventListener("click", function (event) {
+  progressBarAdd();
+  signUp();
+  // displayData();
+  setTimeout(() => {
+    alert(`You are successfuly created your own ticket`);
+  }, 800);
+});
